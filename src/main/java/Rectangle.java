2c59adb4b4d00a3a998f5180f0c2b0f9c1ffbc8b
aboots <mahdi.abootorabi2@gmail.com>
2023-11-12 20:22:44 +0330

    feat: add rectangle setter and getter codes

--- src/main/java/Rectangle.java
+++ src/main/java/Rectangle.java
@@ -9,6 +9,22 @@ public class Rectangle{
         this.height = height;
     }
 
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWidth(){
+        return this.width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getHeight(){
+        return this.height;
+    }
+
     public int area() {
         return this.width * this.height;
     }
