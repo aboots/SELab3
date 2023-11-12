930fd2878654c86a829e7a541a9e8d5a32f7a6da
aboots <mahdi.abootorabi2@gmail.com>
2023-11-12 20:18:21 +0330

    feat: add rectangle setter and getter tests

--- src/test/java/ShapeTest.java
+++ src/test/java/ShapeTest.java
@@ -11,4 +11,34 @@ public class ShapeTest {
         Rectangle rectangle = new Rectangle(8, 10);
         Assert.assertEquals(80, rectangle.area());
     }
+
+    @Test
+    // add test for getter of width 
+    public void testRectangleWidth() {
+        Rectangle rectangle = new Rectangle(8, 10);
+        Assert.assertEquals(8, rectangle.getWidth());
+    }
+
+    @Test
+    // add test for getter of height
+    public void testRectangleHeight() {
+        Rectangle rectangle = new Rectangle(8, 10);
+        Assert.assertEquals(10, rectangle.getHeight());
+    }
+
+    @Test
+    // add test for setter of width
+    public void testRectangleSetWidth() {
+        Rectangle rectangle = new Rectangle(8, 10);
+        rectangle.setWidth(5);
+        Assert.assertEquals(50, rectangle.area());
+    }
+
+    @Test
+    // add test for setter of height
+    public void testRectangleSetHeight() {
+        Rectangle rectangle = new Rectangle(8, 10);
+        rectangle.setHeight(5);
+        Assert.assertEquals(40, rectangle.area());
+    }
 }
